1e217078d6aed086d36ff346d5ceb23a38eef165
Genshin <genshindev325@gmail.com>
2024-08-05 09:29:50 -0400

    Update navbar.tsx

--- rule-webapp/components/navbar.tsx
+++ rule-webapp/components/navbar.tsx
@@ -3,20 +3,37 @@
 'use client';
 
 import React from 'react';
+import vercel from '../public/vercel.svg';
 import { FaHome, FaCalendarAlt, FaComments, FaChartBar, FaBell, FaCog } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <div className="flex flex-col items-center h-full py-8 bg-zinc-700 text-white">
+    <div className="flex flex-col items-center h-full py-10 bg-zinc-700 text-white">
       <div className="flex flex-col items-center space-y-8 flex-grow">
-        <FaHome className="w-6 h-6" />
-        <FaCalendarAlt className="w-6 h-6" />
-        <FaComments className="w-6 h-6" />
-        <FaChartBar className="w-6 h-6" />
+        <button className='hover:text-blue-400 active:text-blue-800'>
+          <div className="w-8 h-8 rounded-full bg-gray-200" />
+          {/* <div className="w-8 h-8 rounded-full bg-[url('../public/vercel.svg')] bg-gray-200" /> */}
+        </button>
+        <button className='hover:text-blue-400 active:text-blue-800'>
+          <FaHome className="w-8 h-8" />
+        </button>
+        <button className='hover:text-blue-400 active:text-blue-800'>
+          <FaCalendarAlt className="w-8 h-8" />
+        </button>
+        <button className='hover:text-blue-400 active:text-blue-800'>
+          <FaComments className="w-8 h-8" />
+        </button>
+        <button className='hover:text-blue-400 active:text-blue-800'>
+          <FaChartBar className="w-8 h-8" />
+        </button>
       </div>
       <div className='flex flex-col items-end space-y-8 mt-auto text-black'>
-        <FaBell className="w-6 h-6" />
-        <FaCog className="w-6 h-6" />
+        <button className='hover:text-blue-400 active:text-blue-800'>
+          <FaBell className="w-8 h-8" />
+        </button>
+        <button className='hover:text-blue-400 active:text-blue-800'>
+          <FaCog className="w-8 h-8" />
+        </button>
       </div>
     </div>
   );
