28acfe3a6c62a1cec607619bf5adb84d61a48783
Genshin <genshindev325@gmail.com>
2024-08-05 19:49:44 -0400

    add setting page

--- rule-webapp/app/page.tsx
+++ rule-webapp/app/page.tsx
@@ -1,10 +1,11 @@
-import SignIn from '../pages/signIn';
+import SignIn from '@/pages/signIn';
 import SignUp from '@/pages/signUp';
 import Dashboard from '@/pages/dashboard';
 import SalesManagement from '@/pages/salesManagement';
+import Setting from '@/pages/setting';
 
 export default function Home() {
   return (
-    <SalesManagement/>
+    <Setting/>
   );
 }
---
+++ rule-webapp/pages/setting.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import React from 'react';
+import Navbar from '../components/navbar';
+
+const Setting = () => {
+  return (
+    <div className="min-h-screen min-w-full flex bg-gray-100">
+      <div className="w-20 bg-gray-800">
+        <Navbar />
+      </div>
+      <div className="w-full p-10">
+        <h1 className="text-3xl font-bold mb-6">setting</h1>
+        <div className='flex flex-row gap-6'>
+          <div className="rounded-lg border-2 w-40 sm:w-48 md:w-56 h-28 sm:h-32 md:h-36 bg-white hover:bg-gray-200 text-center flex flex-col justify-center">
+            Store profile settings
+          </div>
+          <div className="rounded-lg border-2 w-40 sm:w-48 md:w-56 h-28 sm:h-32 md:h-36 bg-white hover:bg-gray-200 text-center flex flex-col justify-center">
+            Password settings
+          </div>
+          <div className="rounded-lg border-2 w-40 sm:w-48 md:w-56 h-28 sm:h-32 md:h-36 bg-white hover:bg-gray-200 text-center flex flex-col justify-center">
+            Credit card settings
+          </div>
+          <div className="rounded-lg border-2 w-40 sm:w-48 md:w-56 h-28 sm:h-32 md:h-36 bg-white hover:bg-gray-200 text-center flex flex-col justify-center">
+            Transfer account settings
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Setting;
