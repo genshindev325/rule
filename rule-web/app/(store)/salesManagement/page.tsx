26708620f31813419245d3e010ce330c3e9f5f32
Genshin <genshindev325@gmail.com>
2024-08-06 10:55:38 -0400

    storeprofilesetting

--- rule-web/app/(store)/salesManagement/page.tsx
+++ rule-web/app/(store)/salesManagement/page.tsx
@@ -14,10 +14,10 @@ const SalesManagement = () => {
         <Navbar />
       </div>
       <div className="w-full p-10">
-        <h1 className="text-3xl font-bold mb-6">sales management</h1>
+        <h1 className="text-3xl font-bold mb-6">販売管理</h1>
         <TotalSales />
         <div className="mt-8">
-          <h3 className='text-sm pt-4'>Period specification</h3>
+          <h3 className='text-sm pt-4'>期間指定</h3>
           <div className='flex flex-row py-4 gap-4'>
             <span className='p-2 border-none rounded-lg bg-gray-300 w-48 text-sm mt-auto'>
               November 14, 2022
--- rule-web/app/(store)/setting/storeProfileSetting/page.tsx
+++ rule-web/app/(store)/setting/storeProfileSetting/page.tsx
@@ -21,49 +21,41 @@ const StoreProfileSettings = () => {
       <div className='w-auto mx-auto'>
         <div className="min-h-screen flex items-start py-20 justify-center bg-gray-100">
           <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
-            <h2 className="text-2xl font-bold mb-6">Store profile settings</h2>
+            <h2 className="text-2xl font-bold mb-6">ストアプロフィール設定</h2>
             <form onSubmit={handleSubmit}>
-              <h3 className='text-gray-600 py-2'>Store name</h3>
+              <h3 className='text-gray-600 py-2'>店名</h3>
               <div className="mb-4">
                 <input
                   type="name"
                   className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
-                  placeholder="Store name"
+                  placeholder="店名"
                 />
               </div>
-              <h3 className='text-gray-600 py-2'>Store genre</h3>
+              <h3 className='text-gray-600 py-2'>店舗ジャンル</h3>
               <div className="mb-4">
                 <input
                   type="name"
                   className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
-                  placeholder="Store genre"
+                  placeholder="店舗ジャンル"
                 />
               </div>
-              <h3 className='text-gray-600 py-2'>Food genre</h3>
+              <h3 className='text-gray-600 py-2'>食べ物のジャンル</h3>
               <div className="mb-4">
                 <input
                   type="name"
                   className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
-                  placeholder="Food genre"
+                  placeholder="食べ物のジャンル"
                 />
               </div>
-              <h3 className='text-gray-600 py-2'>Cooking genre</h3>
+              <h3 className='text-gray-600 py-2'>料理ジャンル</h3>
               <div className="mb-4">
                 <input
                   type="name"
                   className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
-                  placeholder="Cooking genre"
+                  placeholder="料理ジャンル"
                 />
               </div>
-              <h3 className='text-gray-600 py-2'>Cooking genre</h3>
-              <div className="mb-4">
-                <input
-                  type="name"
-                  className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
-                  placeholder="Cooking genre"
-                />
-              </div>
-              <h3 className='text-gray-600 py-2'>address</h3>
+              <h3 className='text-gray-600 py-2'>住所</h3>
               <div className="mb-4">
                 <input
                   type="address"
@@ -71,7 +63,7 @@ const StoreProfileSettings = () => {
                   placeholder="Annie's Building 1F, 1-17-2 Higashi-Shinsaibashi, Chuo-ku, Osaka-shi, Osaka Prefecture"
                 />
               </div>
-              <h3 className='text-gray-600 py-2'>access</h3>
+              <h3 className='text-gray-600 py-2'>アクセス</h3>
               <div className="mb-4">
                 <input
                   type="name"
@@ -88,23 +80,23 @@ const StoreProfileSettings = () => {
                 <button type='button' className='w-10 h-10 rounded-full bg-gray-100 hover:bg-gray-200 font-light text-4xl flex flex-col justify-center items-center pt-3'>+</button>
               </div>
               <div className="mb-4">
-                <h3 className='text-gray-600 py-2'>Store image</h3>
+                <h3 className='text-gray-600 py-2'>ストアイメージ</h3>
               </div>
               <div className="mb-4">
-                <h3 className='text-gray-600 py-2'>Explanatory text</h3>
+                <h3 className='text-gray-600 py-2'>説明文</h3>
                 <textarea
                   className="w-full px-6 mt-3 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
-                  placeholder="Explanation text"
+                  placeholder="説明文"
                   rows={5}
                 />
               </div>
               {/* buttons */}
               <div className='flex flex-row justify-end gap-4 pt-12'>
                 <button type="submit" className="w-48 py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600">
-                  keep
+                  保つ
                 </button>
                 <button type="button" className="w-48 py-2 px-4 bg-gray-300 text-black rounded-md hover:bg-gray-400">
-                  cancel
+                  下書き
                 </button>
               </div>
             </form>
