718b39bbf5c2b6783d05285840c7d45a58235ac4
Genshin <genshindev325@gmail.com>
2024-08-06 10:40:17 -0400

    Update page.tsx

--- rule-web/app/(store)/eventSettings/page.tsx
+++ rule-web/app/(store)/eventSettings/page.tsx
@@ -26,27 +26,27 @@ const EventSettings = () => {
       </div>
       <div className="min-h-screen w-auto pt-20 mx-auto bg-gray-100">
         <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
-          <h2 className="text-2xl font-bold mb-6">Event settings</h2>
+          <h2 className="text-2xl font-bold mb-6">イベント設定</h2>
           <form onSubmit={handleSubmit}>
             {/* buttons */}
             <div className='flex flex-row justify-end gap-4 pt-12'>
               <button type="submit" className="w-48 py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600">
-                keep
+                保つ
               </button>
               <button type="button" className="w-48 py-2 px-4 bg-gray-300 text-black rounded-md hover:bg-gray-400">
-                draft
+                下書き
               </button>
             </div>
             {/* Event settings */}
-            <h3 className='text-gray-600 py-2'>Event name</h3>
+            <h3 className='text-gray-600 py-2'>イベント名</h3>
             <div className="mb-4">
               <input
                 type="name"
                 className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
-                placeholder="Event name"
+                placeholder="イベント名"
               />
             </div>
-            <h3 className='text-gray-600 py-2'>Category</h3>
+            <h3 className='text-gray-600 py-2'>カテゴリー</h3>
             <div className="mb-4"> {/*will be modified*/}
               <select
                 id="category"
@@ -55,23 +55,23 @@ const EventSettings = () => {
                 value={selectedCategory}
                 onChange={handleCategoryChange}
               >
-                <option value="">Please select</option>
-                <option value="category1">Category 1</option>
-                <option value="category2">Category 2</option>
-                <option value="category3">Category 3</option>
+                <option value="">選んでください</option>
+                <option value="category1">カテゴリー 1</option>
+                <option value="category2">カテゴリー 2</option>
+                <option value="category3">カテゴリー 3</option>
               </select>
             </div>
-            <h3 className='text-gray-600 py-2'>Cover image</h3>
+            <h3 className='text-gray-600 py-2'>表紙画像</h3>
             <div className="mb-4">
               <div className='w-full px-6 py-3 bg-gray-100 rounded-md flex flex-row justify-center'>
                 <img src='/image/img_1.png' className='w-2/3' />
               </div>
             </div>
-            <h3 className='text-gray-600 py-2'>Explanatory text</h3>
+            <h3 className='text-gray-600 py-2'>説明文</h3>
             <div className="mb-4">
               <textarea
                 className="w-full px-6 mt-3 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
-                placeholder="Explanation text"
+                placeholder="説明文"
                 rows={5}
               />
             </div>
@@ -79,7 +79,7 @@ const EventSettings = () => {
             <div className='mb-4'>
               <div className='flex gap-4'>
                 <div className='flex-1'>
-                  <h3 className='text-gray-600 py-2'>Schedule</h3>
+                  <h3 className='text-gray-600 py-2'>スケジュール</h3>
                   <input
                     type="name"
                     className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
@@ -87,7 +87,7 @@ const EventSettings = () => {
                   />
                 </div>
                 <div className='flex-1'>
-                  <h3 className='text-gray-600 py-2'>Starting time</h3>
+                  <h3 className='text-gray-600 py-2'>開始時間</h3>
                   <input
                     type="name"
                     className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
@@ -95,7 +95,7 @@ const EventSettings = () => {
                   />
                 </div>
                 <div className='flex-1'>
-                  <h3 className='text-gray-600 py-2'>Ending time</h3>
+                  <h3 className='text-gray-600 py-2'>終了時間</h3>
                   <input
                     type="name"
                     className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
@@ -108,19 +108,19 @@ const EventSettings = () => {
             <div className='mb-4'>
               <div className='flex gap-4'>
                 <div className='flex-1'>
-                  <h3 className='text-gray-600 py-2'>Number of men recruited</h3>
+                  <h3 className='text-gray-600 py-2'>採用された男性の数</h3>
                   <input
                     type="name"
                     className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
-                    placeholder="Number of men recruited"
+                    placeholder="採用された男性の数"
                   />
                 </div>
                 <div className='flex-1'>
-                  <h3 className='text-gray-600 py-2'>Number of women recruited</h3>
+                  <h3 className='text-gray-600 py-2'>採用された女性の数</h3>
                   <input
                     type="name"
                     className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
-                    placeholder="Number of women recruited"
+                    placeholder="採用された女性の数"
                   />
                 </div>
               </div>
@@ -129,19 +129,19 @@ const EventSettings = () => {
             <div className='mb-4'>
               <div className='flex gap-4'>
                 <div className='flex-1'>
-                  <h3 className='text-gray-600 py-2'>Men's rate</h3>
+                  <h3 className='text-gray-600 py-2'>男性料金</h3>
                   <input
                     type="name"
                     className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
-                    placeholder="Men's rate"
+                    placeholder="男性料金"
                   />
                 </div>
                 <div className='flex-1'>
-                  <h3 className='text-gray-600 py-2'>Women's rates</h3>
+                  <h3 className='text-gray-600 py-2'>女性料金</h3>
                   <input
                     type="name"
                     className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
-                    placeholder="Women's rates"
+                    placeholder="女性料金"
                   />
                 </div>
               </div>
@@ -149,10 +149,10 @@ const EventSettings = () => {
             {/* buttons */}
             <div className='flex flex-row justify-end gap-4 pt-12'>
               <button type="submit" className="w-48 py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600">
-                keep
+                保つ
               </button>
               <button type="button" className="w-48 py-2 px-4 bg-gray-300 text-black rounded-md hover:bg-gray-400">
-                draft
+                下書き
               </button>
             </div>
           </form>
