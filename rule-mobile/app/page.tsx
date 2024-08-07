4397feac710c6520d2b0deca88d321d4f1fc297e
Genshin <genshindev325@gmail.com>
2024-08-07 19:04:39 -0400

    registerID

--- rule-mobile/app/page.tsx
+++ rule-mobile/app/page.tsx
@@ -2,9 +2,10 @@ import PasswordResetSend from '@/app/auth/passwordResetSend/page';
 import PasswordReset from '@/app/auth/passwordReset/page';
 import Login from './auth/login/page';
 import LoginWith from './auth/loginWith/page';
+import RegisterID from './auth/registerID/page';
 
 export default function Home() {
   return (
-    <LoginWith/>
+    <RegisterID />
   );
 }
