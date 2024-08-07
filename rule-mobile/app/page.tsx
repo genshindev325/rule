8aae0c769199392a00567bc71c20c4d1322568b8
Genshin <genshindev325@gmail.com>
2024-08-07 19:09:58 -0400

    registerName

--- rule-mobile/app/page.tsx
+++ rule-mobile/app/page.tsx
@@ -3,9 +3,10 @@ import PasswordReset from '@/app/auth/passwordReset/page';
 import Login from './auth/login/page';
 import LoginWith from './auth/loginWith/page';
 import RegisterID from './auth/registerID/page';
+import RegisterName from './auth/registerName/page';
 
 export default function Home() {
   return (
-    <RegisterID />
+    <RegisterName />
   );
 }
