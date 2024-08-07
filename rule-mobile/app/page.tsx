56215749251a7fa414be0b40dd205fa300ed043c
Genshin <genshindev325@gmail.com>
2024-08-07 17:52:52 -0400

    add loginWith

--- rule-mobile/app/page.tsx
+++ rule-mobile/app/page.tsx
@@ -1,9 +1,10 @@
 import PasswordResetSend from '@/app/auth/passwordResetSend/page';
 import PasswordReset from '@/app/auth/passwordReset/page';
 import Login from './auth/login/page';
+import LoginWith from './auth/loginWith/page';
 
 export default function Home() {
   return (
-    <Login/>
+    <LoginWith/>
   );
 }
