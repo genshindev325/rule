2e49cad9c733ddc539639eda1cd3c9cd762eff5b
Genshin <genshindev325@gmail.com>
2024-08-07 16:55:38 -0400

    passwordReset

--- rule-mobile/app/auth/passwordReset/page.tsx
+++ rule-mobile/app/auth/passwordReset/page.tsx
@@ -16,21 +16,27 @@ const PasswordReset: React.FC = () => {
   return (
     <div className="flex items-start justify-center min-h-screen w-screen bg-white">
       <div className="h-32 md:h-48 w-full bg-gradient-to-r from-[#7c5ded] to-[#83d5f7]">
-      <div className="bg-white rounded-3xl shadow-xl px-6 md:px-12 mx-8 md:mx-20 mt-8 md:mt-20">
-        <h2 className="text-3xl font-bold py-12 md:py-20 text-center">パスワード再設定</h2>
+      <div className="bg-white rounded-3xl shadow-xl px-6 md:px-12 mx-8 md:mx-20 mt-12 md:mt-20">
+        <h2 className="text-3xl font-bold py-12 md:py-20 px-12 text-center">パスワードを 再設定してください</h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <input
               type="text"
               className="w-full px-3 py-2 md:px-8 md:py-4 border border-gray-700 rounded-lg"
-              placeholder="メールアドレスもしくは電話番号"
+              placeholder="メールアド"
               value={emailOrPhone}
               onChange={(e) => setEmailOrPhone(e.target.value)}
             />
           </div>
-          <p className="text-md md:text-lg font-semibold text-gray-400">
-            ご登録されているメールアドレスもしくはSMSにて、パスワード再設定用のURLを送信します。
-          </p>
+          <div className="mb-4">
+            <input
+              type="text"
+              className="w-full px-3 py-2 md:px-8 md:py-4 border border-gray-700 rounded-lg"
+              placeholder="パスワード(確認用)"
+              value={emailOrPhone}
+              onChange={(e) => setEmailOrPhone(e.target.value)}
+            />
+          </div>
           <button
             type="submit"
             className="w-full py-2 md:py-4 px-4 mb-16 md:mb-24 mt-16 md:mt-32 bg-gradient-to-r from-[#7c5ded] to-[#83d5f7] text-white rounded-full font-bold"
