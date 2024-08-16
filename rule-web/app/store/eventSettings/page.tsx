1103907604836c26d831a9c48f73c19786dd419c
Genshin <genshindev325@gmail.com>
2024-08-16 11:44:29 -0400

    add createEvent api

--- rule-web/app/store/eventSettings/page.tsx
+++ rule-web/app/store/eventSettings/page.tsx
@@ -3,9 +3,12 @@
 'use client';
 
 import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import Navbar from '@/components/store/navbar';
 
 const EventSettings = () => {
+  const router = useRouter();
+
   const [eventName, setEventName] = useState('');
   const [category, setCategory] = useState('');
   const [selectedCategory, setSelectedCategory] = useState('');
@@ -14,10 +17,35 @@ const EventSettings = () => {
     setSelectedCategory(event.target.value);
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = (async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // Add event settings logic here
-  }
+    const formData = new FormData(e.currentTarget);
+    const eventName = formData.get('eventName');
+    const category = formData.get('category');
+    const description = formData.get('description');
+    const schedule = formData.get('schedule');
+    const startTime = formData.get('startTime');
+    const endTime = formData.get('endTime');
+    const maleTotal = formData.get('maleTotal');
+    const femaleTotal = formData.get('femaleTotal');
+    const maleFee = formData.get('maleFee');
+    const femaleFee = formData.get('femaleFee');
+
+    const response = await fetch('/api/store/createEvent', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ eventName, category, description, schedule, startTime, endTime, maleTotal, femaleTotal, maleFee, femaleFee }),
+    });
+
+    if (response.status === 200) {
+      router.push('/store/dashboard');
+      console.log("Create event success.");
+    } else {
+      console.log(response.status);
+      console.log("Create event failed.");
+    }    
+  })
 
   return (
     <div className="min-h-screen min-w-full flex bg-gray-100">
@@ -31,10 +59,10 @@ const EventSettings = () => {
             {/* buttons */}
             <div className='flex flex-row justify-end gap-4 pt-12'>
               <button type="submit" className="w-48 py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600">
-                保つ
+                保存
               </button>
               <button type="button" className="w-48 py-2 px-4 bg-gray-300 text-black rounded-md hover:bg-gray-400">
-                下書き
+                <a href='/store/dashboard'>下書き</a>
               </button>
             </div>
             {/* Event settings */}
@@ -42,8 +70,10 @@ const EventSettings = () => {
             <div className="mb-4">
               <input
                 type="name"
+                name='eventName'
                 className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
                 placeholder="イベント名"
+                required
               />
             </div>
             <h3 className='text-gray-600 py-2'>カテゴリー</h3>
@@ -54,6 +84,7 @@ const EventSettings = () => {
                 className="block w-full px-6 py-3 text-base bg-gray-100 rounded-md focus:outline-none sm:text-sm"
                 value={selectedCategory}
                 onChange={handleCategoryChange}
+                required
               >
                 <option value="">選んでください</option>
                 <option value="category1">カテゴリー 1</option>
@@ -70,9 +101,11 @@ const EventSettings = () => {
             <h3 className='text-gray-600 py-2'>説明文</h3>
             <div className="mb-4">
               <textarea
+                name='description'
                 className="w-full px-6 mt-3 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
                 placeholder="説明文"
                 rows={5}
+                required
               />
             </div>
             {/* schedule */}
@@ -81,25 +114,31 @@ const EventSettings = () => {
                 <div className='flex-1'>
                   <h3 className='text-gray-600 py-2'>スケジュール</h3>
                   <input
-                    type="name"
+                    type="date"
+                    name='schedule'
                     className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
                     placeholder="November 14, 2023"
+                    required
                   />
                 </div>
                 <div className='flex-1'>
                   <h3 className='text-gray-600 py-2'>開始時間</h3>
                   <input
-                    type="name"
+                    type="time"
+                    name='startTime'
                     className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
                     placeholder="17:00"
+                    required
                   />
                 </div>
                 <div className='flex-1'>
                   <h3 className='text-gray-600 py-2'>終了時間</h3>
                   <input
-                    type="name"
+                    type="time"
+                    name='endTime'
                     className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
                     placeholder="21:00"
+                    required
                   />
                 </div>
               </div>
@@ -111,37 +150,45 @@ const EventSettings = () => {
                   <h3 className='text-gray-600 py-2'>採用された男性の数</h3>
                   <input
                     type="name"
+                    name='maleTotal'
                     className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
                     placeholder="採用された男性の数"
+                    required
                   />
                 </div>
                 <div className='flex-1'>
                   <h3 className='text-gray-600 py-2'>採用された女性の数</h3>
                   <input
                     type="name"
+                    name='femaleTotal'
                     className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
                     placeholder="採用された女性の数"
+                    required
                   />
                 </div>
               </div>
             </div>
-            {/* rate */}
+            {/* fee */}
             <div className='mb-4'>
               <div className='flex gap-4'>
                 <div className='flex-1'>
                   <h3 className='text-gray-600 py-2'>男性料金</h3>
                   <input
                     type="name"
+                    name='maleFee'
                     className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
                     placeholder="男性料金"
+                    required
                   />
                 </div>
                 <div className='flex-1'>
                   <h3 className='text-gray-600 py-2'>女性料金</h3>
                   <input
                     type="name"
+                    name='femaleFee'
                     className="w-full px-6 py-3 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
                     placeholder="女性料金"
+                    required
                   />
                 </div>
               </div>
@@ -152,7 +199,7 @@ const EventSettings = () => {
                 保つ
               </button>
               <button type="button" className="w-48 py-2 px-4 bg-gray-300 text-black rounded-md hover:bg-gray-400">
-                下書き
+                <a href='/store/dashboard'>下書き</a>
               </button>
             </div>
           </form>
