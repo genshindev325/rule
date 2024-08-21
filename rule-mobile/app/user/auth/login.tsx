2eef411cf075774cbbd92096425464e865dee4ce
Genshin <genshindev325@gmail.com>
2024-08-21 12:29:59 -0400

    test HTTP request from another port

--- rule-mobile/app/user/auth/login.tsx
+++ rule-mobile/app/user/auth/login.tsx
@@ -4,15 +4,44 @@
 
 import React, { useState } from 'react';
 import { IonPage, IonContent, IonRouterLink } from '@ionic/react';
+import { useRouter } from 'next/navigation';
 
 const Login: React.FC = () => {
+  const router = useRouter();
+
   const [emailAddress, setEmailAddress] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // Handle the form submission
-    console.log(emailAddress);
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    console.log('userName: ' + email);
+    console.log('userEmail: ' + password);
+
+    const response = await fetch('http://localhost:3000/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (response.status === 200) {
+      const result = await response.json();
+      console.log(result.message);
+      const userName = result.userName;
+      const userEmail = result.userEmail;
+      const userRole = result.userRole;
+      const token = result.token;
+      if (userRole === 'user') {
+        console.log('userName: ' + userName);
+        console.log('userEmail: ' + userEmail);
+      }
+    } else {
+      console.log(response.status);
+      console.log("Your username and pasword mismatched.");
+    }
   };
 
   return (
@@ -25,20 +54,24 @@ const Login: React.FC = () => {
             <form onSubmit={handleSubmit}>
               <div className="mb-4">
                 <input
-                  type="text"
+                  type="address"
+                  name='email'
                   className="w-full px-3 py-2 md:px-8 md:py-4 border border-gray-700 rounded-lg"
                   placeholder="メールアドレス"
                   value={emailAddress}
                   onChange={(e) => setEmailAddress(e.target.value)}
+                  required
                 />
               </div>
               <div className="mb-4">
                 <input
-                  type="text"
+                  type="password"
+                  name='password'
                   className="w-full px-3 py-2 md:px-8 md:py-4 border border-gray-700 rounded-lg"
                   placeholder="パスワード"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
+                  required
                 />
               </div>
               <IonRouterLink routerLink='/auth/passwordReset'>
