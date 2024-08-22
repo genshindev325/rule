a896cb1eb878845152eece811cb774f828256735
Genshin <genshindev325@gmail.com>
2024-08-22 12:09:58 -0400

    Update page.tsx

--- rule-web/app/page.tsx
+++ rule-web/app/page.tsx
@@ -1,7 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux';
 
 // Auth
-import SignIn from '@/app/auth/signIn/page';
+import SignIn from '@/app/auth/signin/page';
 import SignUp from '@/app/auth/signUp/page';
 
 // Admin
