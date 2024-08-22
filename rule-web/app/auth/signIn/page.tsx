2acc23dff6ba4847a21dc11fb39e9d651050472c
Genshin <genshindev325@gmail.com>
2024-08-22 12:12:21 -0400

    Update page.tsx

--- rule-web/app/auth/signIn/page.tsx
+++ rule-web/app/auth/signIn/page.tsx
@@ -16,7 +16,7 @@ const SignIn = () => {
     const email = formData.get('email');
     const password = formData.get('password');
 
-    const response = await fetch('/api/auth/login', {
+    const response = await fetch('/api/auth/signin', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),
