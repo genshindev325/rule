db678a9772df2ba3c3c03e63f029357062dcbd5a
Genshin <genshindev325@gmail.com>
2024-08-23 12:57:40 -0400

    fix(signin, signup): connect server and db
    Co-Authored-By: CodeArtist <133830857+beauty-force@users.noreply.github.com>
    Co-Authored-By: GENTLE <3442766+gentleman9999@users.noreply.github.com>

--- rule-mobile/app/store/features/auth/AuthSlice.tsx
+++ rule-mobile/app/store/features/auth/AuthSlice.tsx
@@ -2,33 +2,37 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { setItem, getItem, removeItem } from '@/app/components/utils/storage';
 
+interface ProfileProps {
+  [key: string] : any
+}
+
 interface AuthState {
   isAuthenticated: boolean;
-  userRole: string | null;
-  userEmail: string | null;
-  userName: string | null;
+  role: string | null;
+  email: string | null;
+  profile: ProfileProps | null;
   token: string | null;
 }
 
 let isAuthenticated = false;
-let userRole = '';
-let userEmail = '';
-let userName = '';
+let role = '';
+let email = '';
+let profile = '';
 let token = '';
 
 async () => {
   isAuthenticated = await getItem('isAuthenticated');
-  userRole = await getItem('userRole');
-  userEmail = await getItem('userEmail');
-  userName = await getItem('userName');
+  role = await getItem('role');
+  email = await getItem('email');
+  profile = await getItem('profile');
   token = await getItem('token');
 }
 
 const initialState: AuthState = {
   isAuthenticated: isAuthenticated || false,
-  userRole: userRole || null,
-  userEmail: userEmail || null,
-  userName: userName || null,
+  role: role || null,
+  email: email || null,
+  profile: profile && JSON.parse(profile) || null,
   token: token || null
 };
 
@@ -38,29 +42,29 @@ const authSlice = createSlice({
   reducers: {
     signIn: (state, action: PayloadAction<{ user: any; token: string }>) => {
       state.isAuthenticated = true;
-      state.userRole = action.payload.user.userRole;
-      state.userEmail = action.payload.user.userEmail;
-      state.userName = action.payload.user.userName;
+      state.role = action.payload.user.userRole;
+      state.email = action.payload.user.email;
+      state.profile = action.payload.user.profile;
       state.token = action.payload.token;
 
       setItem('isAuthenticated', JSON.stringify(true));
       setItem('token', action.payload.token);
-      setItem('userName', action.payload.user.userName);
-      setItem('userEmail', action.payload.user.userEmail);
-      setItem('userRole', action.payload.user.userRole);
+      setItem('profile', JSON.stringify(action.payload.user.profile));
+      setItem('email', action.payload.user.email);
+      setItem('role', action.payload.user.role);
     },
     signOut: (state) => {
       state.isAuthenticated = false;
-      state.userRole = null;
-      state.userEmail = null;
-      state.userName = null;
+      state.role = null;
+      state.email = null;
+      state.profile = null;
       state.token = null;
 
       setItem('isAuthenticated', JSON.stringify(false));
       removeItem('token');
-      removeItem('userName');
-      removeItem('userEmail');
-      removeItem('userRole');
+      removeItem('profile');
+      removeItem('email');
+      removeItem('role');
     },
   },
 });
