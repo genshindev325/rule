24797ea92d3f42a59746a50e10271c7c4c92b0b0
Genshin <genshindev325@gmail.com>
2024-08-26 10:05:37 -0400

    fix(eventCard): fix date time format
    Co-Authored-By: GENTLE <3442766+gentleman9999@users.noreply.github.com>
    Co-Authored-By: CodeArtist <133830857+beauty-force@users.noreply.github.com>

--- rule-mobile/app/components/user/event/eventCard.tsx
+++ rule-mobile/app/components/user/event/eventCard.tsx
@@ -1,6 +1,7 @@
 // components/event/eventCard.tsx
 
 import React, { useState } from 'react';
+import { formatDateTime } from '@/app/components/utils/datetime';
 
 // will be modified
 interface CardProps {
@@ -29,12 +30,12 @@ const EventCard: React.FC<CardProps> = ({
       <img src={coverImage} alt={`event-profile`} className="rounded-md rounded-br-none w-24 sm:w-36 h-20 sm:h-24" />
       <div className='flex flex-col space-y-1'>
         <h2 className="text-xs sm:text-sm md:text-md font-bold">{eventName}</h2>
-        <h2 className="text-xs sm:text-sm md:text-md">{eventDate}</h2>
+        <h2 className="text-xs sm:text-sm md:text-md">{formatDateTime(eventDate)}</h2>
         <div className='flex flex-row space-x-1'>
           <div className={`${maleGradient} px-1 rounded-full w-10 text-center text-xs sm:text-sm md:text-md text-white my-auto`}>男性</div>
           <h2 className='text-xs sm:text-sm md:text-md'>{maleFee}円 募集 : {males}/{maleTotal}</h2>
           <div className="w-16 md:w-28 bg-gray-300 h-2 rounded-full my-auto">
-            <div 
+            <div
               className={`h-2 ${maleGradient}`} 
               style={{ width: `${maleRate * 100}%` }}
             ></div>
