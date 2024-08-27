cf35b9ef89493396ba7c8f342144303dadc448dd
GENTLE <gentleman99729@outlook.com>
2024-08-28 07:27:01 +0900

    feat(api): apis for store dashboard information

---
+++ rule-web/src/app/api/stores/main-panel/route.tsx
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import dbConnect from '@/lib/mongoose';
+import Store from '@/models/storeModel';
+
+export async function GET(req: NextRequest) {
+    //await dbConnect();
+    
+    const body = await req.json();
+    const { email } = body;
+
+    try {
+        // Process a GET request
+        const lastMonthSales = 333234;
+        const thisMonthSales = 23456;
+        const scheduledEvents = 3;
+        const unreachedCases =1;
+        const reviews = 12;
+        const reviewResponseRate = 80;
+        
+        return NextResponse.json({
+            lastMonthSales: lastMonthSales,
+            thisMonthSales: thisMonthSales,
+            scheduledEvents: scheduledEvents,
+            unreachedCases: unreachedCases,
+            reviews: reviews,
+            reviewResponseRate: reviewResponseRate
+            }, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ success: false, error }, { status: 500 });
+    }
+}
---
+++ rule-web/src/app/api/stores/recent-reviews/route.tsx
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import dbConnect from '@/lib/mongoose';
+import Store from '@/models/storeModel';
+
+export async function GET(req: NextRequest) {
+    //await dbConnect();
+    
+    const body = await req.json();
+    const { email } = body;
+
+    try {
+        const recentReviews = [
+            {
+              user: '橋本バナナ',
+              date: '昨日',
+              content: 'とても楽しかったです！機会があればまた参加したいです。',
+              conclusion: '食事も美味しかったです。',
+              rating: 5
+            },
+            {
+              user: '橋本バナナ',
+              date: '昨日',
+              content: 'とても楽しかったです！機会があればまた参加したいです。',
+              conclusion: '食事も美味しかったです。',
+              rating: 4
+            },
+            {
+              user: '橋本バナナ',
+              date: '昨日',
+              content: 'とても楽しかったです！機会があればまた参加したいです。',
+              conclusion: '食事も美味しかったです。',
+              rating: 5
+            },
+            // Add more reviews as needed
+        ];
+        
+        return NextResponse.json({
+            recentReviews: recentReviews
+            }, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ success: false, error }, { status: 500 });
+    }
+}
