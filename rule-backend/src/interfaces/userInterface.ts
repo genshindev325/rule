28c3c4e9a22fdf2a9d7c322a647413c05a791d83
Genshin <genshindev325@gmail.com>
2024-08-28 06:09:43 -0400

    chore: delete rule-backend
    Co-Authored-By: GENTLE <3442766+gentleman9999@users.noreply.github.com>
    Co-Authored-By: CodeArtist <133830857+beauty-force@users.noreply.github.com>

--- rule-backend/src/interfaces/userInterface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from "mongoose";
-
-export interface IUser extends Document {
-    username: string;
-    email: string;
-    password: string;
-    role: string;
-    comparePassword: (password: string) => Promise<boolean>;
-}
--- rule-backend/src/utils/validateEnv.ts
+++
@@ -1,9 +0,0 @@
-import { cleanEnv, str, port } from "envalid";
-
-export const validateEnv = () => {
-  cleanEnv(process.env, {
-    MONGO_URI: str(),
-    JWT_SECRET: str(),
-    PORT: port({ default: 5000 }),
-  });
-};
