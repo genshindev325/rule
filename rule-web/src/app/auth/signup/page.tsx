86397b05d00ca3b69230bc04dfe0a76942f3a941
Genshin <genshindev325@gmail.com>
2024-08-30 12:34:56 -0400

    test
    Co-Authored-By: GENTLE <3442766+gentleman9999@users.noreply.github.com>
    Co-Authored-By: CodeArtist <133830857+beauty-force@users.noreply.github.com>

---
+++ rule-web/src/app/auth/signup/page.tsx
@@ -0,0 +1,171 @@
+// pages/signUp.tsx
+'use client';
+
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuth } from '@/components/auth/authContext';
+
+const SignUp = () => {
+  const router = useRouter();
+  const { signin } = useAuth();
+
+  // form validation
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [confirmError, setConfirmError] = useState('');
+  const minLength = 6;
+  const maxLength = 20;
+
+  const validateEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validatePassword = (newPassword: string) => {
+    if (newPassword.length < minLength) {
+      return `パスワードは${minLength}文字以上でなければなりません。`;
+    } else if (newPassword.length > maxLength) {
+      return `パスワードは${maxLength}文字を超えることはできません。`;
+    } else if (!/[A-Z]/.test(newPassword)) {
+      return 'パスワードには少なくとも 1 つの大文字が含まれている必要があります。';
+    } else if (!/[a-z]/.test(newPassword)) {
+      return 'パスワードには少なくとも 1 つの小文字が含まれている必要があります。';
+    } else if (!/[0-9]/.test(newPassword)) {
+      return 'パスワードには少なくとも 1 つの数字を含める必要があります。';
+    // } else if (!/[!@#$%^&*]/.test(newPassword)) {
+    //   return 'パスワードには少なくとも1つの特殊文字を含める必要があります (!@#$%^&*).';
+    } else {
+      return '';
+    }
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newEmail = e.target.value;
+    setEmail(newEmail);
+
+    if (!validateEmail(newEmail)) {
+      setEmailError('有効なメールアドレスを入力してください。');
+    } else {
+      setEmailError('');
+    }
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newPassword = e.target.value;
+    setPassword(newPassword);
+    setPasswordError(validatePassword(newPassword));
+
+    if (confirmPassword && newPassword !== confirmPassword) {
+      setConfirmError('パスワードが一致しません。');
+    } else {
+      setConfirmError('');
+    }
+  };
+
+  const handleConfirmPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newConfirmPassword = e.target.value;
+    setConfirmPassword(newConfirmPassword);
+
+    if (newConfirmPassword !== password) {
+      setConfirmError('パスワードが一致しません。');
+    } else {
+      setConfirmError('');
+    }
+  };
+
+  // handle submit
+  const handleSubmit = (async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Add sign-up logic here
+    const formData = new FormData(e.currentTarget);
+    const username = formData.get('username');
+
+    const response = await fetch('/api/stores', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, username, password }),
+    });
+
+    if (response.status === 201) {
+      const result = await response.json();
+      const {
+        email,
+        role,
+        profile,
+        token
+      } = result.data;
+
+      signin(email, role, profile, token);
+      router.push('/store/dashboard');
+    } else {
+      // Error handler
+      console.log(response.status);
+    }
+  });
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+        <h2 className="text-2xl font-bold mb-6">サインアップ</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <input
+              type="text"
+              value={email}
+              onChange={handleEmailChange}
+              className="w-full px-6 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+              placeholder="メール"
+              required
+            />
+            {emailError && <p className="text-red-500 mt-2">{emailError}</p>}
+          </div>
+          <div className="mb-4">
+            <input
+              type="text"
+              name='username'
+              className="w-full px-6 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+              placeholder="ユーザー名"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+              className="w-full px-6 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+              placeholder="パスワード"
+            />
+            {passwordError && <p className="text-red-500 mt-2">{passwordError}</p>}
+          </div>
+          <div className="mb-4">
+            <input
+              type="password"
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
+              className="w-full px-6 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+              placeholder="パスワードの確認"
+            />
+            {confirmError && <p className="text-red-500 mt-2">{confirmError}</p>}
+          </div>
+          <button
+            type="submit"
+            className="w-full mt-10 py-2 px-4 bg-black text-white rounded-md hover:bg-gray-800 focus:outline-none focus:ring focus:border-blue-300"
+          >
+            サインアップ
+          </button>
+          <div className='my-6 text-right'>
+            <a href='/auth/signin' className='text-sm text-blue-300 hover:text-blue-500 hover:cursor-pointer font-bold'>
+              すでにアカウントを持っています
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
