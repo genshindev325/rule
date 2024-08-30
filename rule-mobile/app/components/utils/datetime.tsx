e5cbdbed0f31e59b87e24fd9fbdff6979b2f523f
Genshin <genshindev325@gmail.com>
2024-08-30 16:59:16 -0400

    fix
    Co-Authored-By: GENTLE <3442766+gentleman9999@users.noreply.github.com>
    Co-Authored-By: CodeArtist <133830857+beauty-force@users.noreply.github.com>

--- rule-mobile/app/components/utils/datetime.tsx
+++ rule-mobile/app/components/utils/datetime.tsx
@@ -1,10 +1,9 @@
 export const formatDateTime = ( dt: string ) => {
-    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
     const curDateTime = new Date(dt);
     const y = curDateTime.getFullYear();
-    const m = months[curDateTime.getMonth()];
+    const m = curDateTime.getMonth() + 1;
     const d = curDateTime.getDate();
     const mm = curDateTime.getMinutes();
     const hh = curDateTime.getHours();
-    return `${m} ${d}, ${y} ${hh}:${mm}`;
+    return `${y}年 ${m}月 ${d}日 ${hh}:${mm}`;
 }
--- rule-mobile/app/config.tsx
+++ rule-mobile/app/config.tsx
@@ -1,1 +1,1 @@
-export const SERVER_URL = 'https://rule-izakaya.vercel.app/';
+export const SERVER_URL = 'https://rule-izakaya.vercel.app';
