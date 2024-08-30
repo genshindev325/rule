f9845670702d9a064ad425ab8e9adb79f0e07032
Genshin <genshindev325@gmail.com>
2024-08-30 11:20:21 -0400

    Update next.config.mjs
    Co-Authored-By: GENTLE <3442766+gentleman9999@users.noreply.github.com>
    Co-Authored-By: CodeArtist <133830857+beauty-force@users.noreply.github.com>

--- rule-web/next.config.mjs
+++ rule-web/next.config.mjs
@@ -1,36 +1,33 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-// Derive __dirname from import.meta.url
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/api/(.*)",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
-  webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    return config;
-  },
+    async headers() {
+        return [
+            {
+                // Routes this applies to
+                source: "/api/(.*)",
+                // Headers
+                headers: [
+                // Allow for specific domains to have access or * for all
+                {
+                    key: "Access-Control-Allow-Origin",
+                    value: "*",
+                    // DOES NOT WORK
+                    // value: process.env.ALLOWED_ORIGIN,
+                },
+                // Allows for specific methods accepted
+                {
+                    key: "Access-Control-Allow-Methods",
+                    value: "GET, POST, PUT, DELETE, OPTIONS",
+                },
+                // Allows for specific headers accepted (These are a few standard ones)
+                {
+                    key: "Access-Control-Allow-Headers",
+                    value: "Content-Type, Authorization",
+                },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
