917fb6d719e9462e868da741a06a6209515c954c
Genshin <genshindev325@gmail.com>
2024-09-05 21:07:46 -0400

    fix: change email into nickname in review in store dashboard
    Co-Authored-By: GENTLE <3442766+gentleman9999@users.noreply.github.com>
    Co-Authored-By: CodeArtist <133830857+beauty-force@users.noreply.github.com>

--- rule-web/src/components/store/dashboard/recentReviews.tsx
+++ rule-web/src/components/store/dashboard/recentReviews.tsx
@@ -35,7 +35,7 @@ const RecentReviews: React.FC<RecentReviewsProps> = ({ onSeeMore, reviews }) => 
                   <img src={review.createdBy.avatar} className='rounded-full w-10 h-10' />
                 </div>
               <div className="ml-2">
-                <div className="font-semibold">{review.createdBy.email}</div>
+                <div className="font-semibold">{review.createdBy.nickname}</div>
               </div>
             </div>
             <div className="flex items-center mb-2">
