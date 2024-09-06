6e6b59e220b8406c9004409e400d45cd5ccb484b
Genshin <genshindev325@gmail.com>
2024-09-05 22:09:25 -0400

    Update recentReviews.tsx
    Co-Authored-By: GENTLE <3442766+gentleman9999@users.noreply.github.com>
    Co-Authored-By: CodeArtist <133830857+beauty-force@users.noreply.github.com>

--- rule-web/src/components/store/dashboard/recentReviews.tsx
+++ rule-web/src/components/store/dashboard/recentReviews.tsx
@@ -20,10 +20,11 @@ interface RecentReview {
 
 interface RecentReviewsProps {
   onSeeMore: () => void;
+  onSelectReview: (review: RecentReview) => void;
   reviews: RecentReview[]
 }
 
-const RecentReviews: React.FC<RecentReviewsProps> = ({ onSeeMore, reviews }) => {
+const RecentReviews: React.FC<RecentReviewsProps> = ({ onSeeMore, reviews, onSelectReview }) => {
   return (
     <div className="p-4 bg-white shadow-md rounded-md min-w-80">
       <h3 className="text-lg font-semibold mb-4">最近のレビュー</h3>
@@ -48,13 +49,17 @@ const RecentReviews: React.FC<RecentReviewsProps> = ({ onSeeMore, reviews }) => 
             </div>
             <p>{review.storeReviewText}</p>
             <div className='text-sm text-gray-500 mt-4'>{review.conclusion}</div>
-            <div className="text-md text-gray-400 mt-4 mb-10 cursor-pointer">返事する</div>
+            <div className="text-md text-gray-400 mt-4 mb-10 cursor-pointer">
+              <span onClick={() => onSelectReview(review)}>
+                返事する
+              </span>
+            </div>
           </li>
         )) : <p className='text-center py-10'>まだレビューはありません。</p>}
         <div className='text-center mt-8 mb-2'>
           {reviews &&
           <button id="seeMore" className='p-2 border-none rounded-lg bg-white hover:bg-gray-300' onClick={onSeeMore}>
-            続きを見る
+            もっと見る
           </button>}
         </div>
       </ul>
