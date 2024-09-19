592af03b8a72bf97b17f52af545562f8a78c2f24
Genshin <genshindev325@gmail.com>
2024-09-19 13:22:40 -0400

    Update eventParticipateModel.ts
    Co-Authored-By: CodeArtist <133830857+beauty-force@users.noreply.github.com>
    Co-Authored-By: GENTLE <3442766+gentleman9999@users.noreply.github.com>

--- rule-web/src/models/eventParticipateModel.ts
+++ rule-web/src/models/eventParticipateModel.ts
@@ -3,7 +3,7 @@ import mongoose, { Document, Schema, Model } from 'mongoose';
 interface IEventParticipate extends Document {
     userId: mongoose.ObjectId;
     eventId: mongoose.ObjectId;
-    fee: number;
+    totalPrice: number;
     status: string;
     createdAt: Date;
 }
@@ -11,7 +11,7 @@ interface IEventParticipate extends Document {
 const eventParticipateSchema = new Schema<IEventParticipate>({
     userId: { type: Schema.Types.ObjectId, require:true, ref: 'User' },
     eventId: { type: Schema.Types.ObjectId, require:true, ref: 'Event' },
-    fee: { type:Number, default: 0 },
+    totalPrice: { type:Number, default: 0 },
     status: { type: String },
 
     createdAt: { type: Date, default: () => new Date() },
