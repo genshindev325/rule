5efa842dc7d0f0decdc462ced68a3cc0b947bc60
Genshin <genshindev325@gmail.com>
2024-09-26 17:20:33 -0400

    test
    Co-Authored-By: GENTLE <3442766+gentleman9999@users.noreply.github.com>
    Co-Authored-By: CodeArtist <133830857+beauty-force@users.noreply.github.com>

--- rule-mobile/app/components/user/event/findDetailModal.tsx
+++ rule-mobile/app/components/user/event/findDetailModal.tsx
@@ -8,6 +8,15 @@ import { SERVER_URL } from '@/app/config';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/app/store/store';
 
+// Get today's date in the YYYY-MM-DD format
+const today = new Date();
+const getTodayDate = (): string => {
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
+
 interface EventProps {
   _id: string,
   eventName: string,
@@ -49,7 +58,7 @@ const FindDetailModal: React.FC<ReviewModalProps> = ({ isOpen, onClose }) => {
 
   const modalRef = useRef<HTMLDivElement>(null);
   const [location, setLocation] = useState('');
-  const [date, setDate] = useState('');
+  const [date, setDate] = useState(getTodayDate());
   const [gender, setGender] = useState<'male' | 'female'>('male');
   const [age, setAge] = useState('');
   const [category, setCategory] = useState('');
@@ -88,34 +97,6 @@ const FindDetailModal: React.FC<ReviewModalProps> = ({ isOpen, onClose }) => {
     }
   };
 
-  const handleLocationChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setLocation(e.target.value);
-  };
-
-  const handleDateChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setDate(e.target.value);
-  };
-
-  const handleAgeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setAge(e.target.value);
-  };
-
-  const handleCategoryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setCategory(event.target.value);
-  };
-
-  const handleStoreGenreChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setStoreGenre(e.target.value);
-  };
-
-  const handleFoodGenreChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setFoodGenre(e.target.value);
-  };
-
-  const handleCookingGenreChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setCookingGenre(e.target.value);
-  };
-
   const handleClickOutside = (event: MouseEvent) => {
     if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
       onClose();
@@ -140,21 +121,21 @@ const FindDetailModal: React.FC<ReviewModalProps> = ({ isOpen, onClose }) => {
       <div ref={modalRef} className="bg-white px-4 py-6 rounded-2xl shadow-md w-[85%] max-w-2xl mx-4 sm:mx-8">
         {/*location and date*/}
         <div className="flex mb-3 space-x-2">
-          <select id="location" name="location" value={location} onChange={handleLocationChange}
+          <select id="location" name="location" value={location} onChange={(e) => setLocation(e.target.value)}
             className={`block w-40 p-2 bg-transparent border-solid border border-gray-500 rounded-md focus:outline-none ${textXs}`}
           >
             <option value="">場所を選択</option>
             <option value="Tokyo, Japan">東京、日本</option>
             <option value="Osaka, Japan">大阪、日本</option>
           </select>
-          <select id="date" name="date" value={date} onChange={handleDateChange}
-            className={`block w-40 p-2 bg-transparent border-solid border border-gray-500 rounded-md focus:outline-none ${textXs}`}
-          >
-            <option value="">日付を選択</option>
-            <option value="date1">日付 1</option>
-            <option value="date2">日付 2</option>
-            <option value="date3">日付 3</option>
-          </select>
+          <input
+            type="date"
+            name='date'
+            onChange={(e) => setDate(e.target.value)}
+            className={`w-40 p-2 bg-transparent rounded-md focus:outline-none border border-solid border-gray-500 ${textXs}`}
+            value={date}
+            min={new Date().toISOString().split('T')[0]}
+          />
         </div>
         {/*gender and age*/}
         <div className="flex space-x-2">
@@ -162,69 +143,74 @@ const FindDetailModal: React.FC<ReviewModalProps> = ({ isOpen, onClose }) => {
             <button className={`rounded-l-md px-5 py-2 border border-r-0 ${textXs} text-center ${gender === 'male' ? maleGradient + ' text-white border-none' : 'bg-transparent text-black border-solid border-gray-500'}`} onClick={() => setGender('male')}>男性</button>
             <button className={`rounded-r-md px-5 py-2 border border-l-0 ${textXs} text-center ${gender === 'female' ? femaleGradient + ' text-white border-none' : 'bg-transparent text-black border-solid border-gray-500'}`} onClick={() => setGender('female')}>女性</button>
           </div>
-          <select id="age" name="age" value={age} onChange={handleAgeChange}
+          <select id="age" name="age" value={age} onChange={(e) => setAge(e.target.value)}
             className={`block w-28 p-2 bg-transparent border-solid border border-gray-500 rounded-md focus:outline-none ${textXs}`}
           >
             <option value="">年齢</option>
-            <option value="age1">20代以上</option>
-            <option value="age2">30代以上</option>
-            <option value="age3">40代以上</option>
-            <option value="age3">50代以上</option>
-            <option value="age3">60代以上</option>
+            <option value="20代">20代</option>
+            <option value="30代">30代</option>
+            <option value="40代">40代</option>
+            <option value="50代">50代</option>
+            <option value="60代以上">60代以上</option>
           </select>
           <h2 className={`${textXs} my-auto`}>歳</h2>
         </div>
         {/* category, store, food, genre */}
         <div className='flex flex-col space-y-3 py-3'>
-          {/* <div className='relative'>
-            <input
-              type="text"
-              name={`access${index}`}
-              value={value}
-              onChange={(e) => handleAccessChange(index, e.target.value)}
-              className="w-full p-2 mb-2 bg-gray-100 rounded-md focus:outline-none focus:border-blue-100"
-              placeholder={`大阪メトロ 御堂筋線 心斎橋駅から徒歩10分`}
-            />
-            <button
-              type="button"
-              onClick={() => handleRemoveAccess(index)}
-              className="absolute right-2 top-5 hover:font-bold duration-300 transform -translate-y-1/2 text-gray-500 hover:text-red-700"
-            >
-              +
-            </button>
-          </div> */}
-          <select id="category" name="category" value={category} onChange={handleCategoryChange}
+          <select id="category" name="category" value={category} onChange={(e) => setCategory(e.target.value)}
             className={`block w-full p-2 bg-transparent border-solid border border-gray-500 rounded-md focus:outline-none ${textXs}`}
           >
             <option value="">カテゴリーを選択</option>
-            <option value="category1">10</option>
-            <option value="category2">20</option>
-            <option value="category3">30</option>
+            <option value="ランチ">ランチ</option>
+            <option value="ディナー">ディナー</option>
+            <option value="合コン">合コン</option>
+            <option value="婚活">婚活</option>
+            <option value="趣味交流会">趣味交流会</option>
+            <option value="その他">その他 ...</option>
           </select>
-          <select id="store" name="store" value={storeGenre} onChange={handleStoreGenreChange}
+          <select id="store" name="store" value={storeGenre} onChange={(e) => setStoreGenre(e.target.value)}
             className={`block w-full p-2 bg-transparent border-solid border border-gray-500 rounded-md focus:outline-none ${textXs}`}
           >
             <option value="">店舗ジャンルを選択</option>
-            <option value="wedding, drinking, party">10</option>
-            <option value="store2">20</option>
-            <option value="store3">30</option>
+            <option value="カフェ">カフェ</option>
+            <option value="居酒屋">居酒屋</option>
+            <option value="レストラン">レストラン</option>
+            <option value="和食">和食</option>
+            <option value="バー">バー</option>
+            <option value="ラウンジ">ラウンジ</option>
+            <option value="屋外ガーデン">屋外ガーデン</option>
+            <option value="専門料理店">専門料理店</option>
+            <option value="その他">その他 ...</option>
           </select>
-          <select id="food" name="food" value={foodGenre} onChange={handleFoodGenreChange}
+          <select id="food" name="food" value={foodGenre} onChange={(e) => setFoodGenre(e.target.value)}
             className={`relative w-full p-2 bg-transparent border-solid border border-gray-500 rounded-md focus:outline-none ${textXs}`}
           >
             <option value="">食材ジャンルを選択</option>
-            <option value="food1">10</option>
-            <option value="food2">20</option>
-            <option value="food3">30</option>
+            <option value="肉系">肉系</option>
+            <option value="魚介系">魚介系</option>
+            <option value="野菜系">野菜系</option>
+            <option value="その他">その他 ...</option>
           </select>
-          <select id="cooking" name="cooking" value={cookingGenre} onChange={handleCookingGenreChange}
+          <select id="cooking" name="cooking" value={cookingGenre} onChange={(e) => setCookingGenre(e.target.value)}
             className={`block w-full p-2 bg-transparent border-solid border border-gray-500 rounded-md focus:outline-none ${textXs}`}
           >
             <option value="">料理ジャンルを選択</option>
-            <option value="genre1">和食</option>
-            <option value="genre2">洋食</option>
-            <option value="genre3">華食</option>
-            <option value="genre4">居酒屋</option>
+            <option value="和食">和食</option>
+            <option value="焼肉">焼肉</option>
+            <option value="寿司">寿司</option>
+            <option value="天ぷら">天ぷら</option>
+            <option value="しゃぶしゃぶ">しゃぶしゃぶ</option>
+            <option value="フレンチ">フレンチ</option>
+            <option value="イタリアン">イタリアン</option>
+            <option value="中華料理">中華料理</option>
+            <option value="韓国料理">韓国料理</option>
+            <option value="地中海料理">地中海料理</option>
+            <option value="エスニック料理">エスニック料理</option>
+            <option value="タイ料理">タイ料理</option>
+            <option value="インド料理">インド料理</option>
+            <option value="ヴィーガン・ベジタリアン料理">ヴィーガン・ベジタリアン料理</option>
+            <option value="創作料理">創作料理</option>
+            <option value="その他">その他 ...</option>
           </select>
         </div>
         <button type='submit' id='searchWith' className={`rounded-full w-full py-2 mt-6 ${maleGradient} ${textSm} font-semibold text-white`} onClick={handleSubmit}>この条件で検索</button>
--- rule-web/src/app/api/events/filter/route.tsx
+++ rule-web/src/app/api/events/filter/route.tsx
@@ -16,14 +16,17 @@ export async function POST(req: NextRequest) {
   try {
     let query = Event.find();
     if (body.upcoming) {
+      console.log('upcoming')
       query.where("eventDate").gte(new Date().getTime());
       query.populate({
         path: 'store',
         select: 'storeLat storeLng storeName storeImages address access description status cookingGenre foodGenre storeGenre'
       })
-    } else if (body.openAt) {
-      query.where("eventDate").equals(body.openAt);
+    } else if (body.date) {
+      console.log('date')
+      query.where("eventDate").equals(body.date);
     } else if (body.past) {
+      console.log('past')
       // find past events user participated
       if (body.user) {
         // Find all event participations by the user
@@ -50,10 +53,12 @@ export async function POST(req: NextRequest) {
     }
 
     if (body.category) {
+      console.log('category')
       query.where("category").equals(body.category);
     }
 
     if (body.location) {
+      console.log('location')
       query.populate({
         path: 'store',
         options: { address: body.location },
@@ -62,6 +67,7 @@ export async function POST(req: NextRequest) {
     }
 
     if (body.storeGenre) {
+      console.log('storeGenre')
       query.populate({
         path: 'store',
         match: { storeGenre: body.storeGenre },
@@ -70,6 +76,7 @@ export async function POST(req: NextRequest) {
     }
 
     if (body.foodGenre) {
+      console.log('foodGenre')
       query.populate({
         path: 'store',
         match: { foodGenre: body.foodGenre },
@@ -78,6 +85,7 @@ export async function POST(req: NextRequest) {
     }
 
     if (body.cookingGenre) {
+      console.log('cookingGenre')
       query.populate({
         path: 'store',
         match: { cookingGenre: body.cookingGenre },
@@ -90,6 +98,7 @@ export async function POST(req: NextRequest) {
     }
 
     const events = await query.exec();
+    console.log("events: " + JSON.stringify(events))
     return NextResponse.json({ success: true, data: events }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ success: false, error }, { status: 500 });
