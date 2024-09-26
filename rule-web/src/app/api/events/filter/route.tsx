80fb170115ab74c8676070eba585a3a0a9077c39
Genshin <genshindev325@gmail.com>
2024-09-26 17:38:18 -0400

    Update route.tsx
    Co-Authored-By: GENTLE <3442766+gentleman9999@users.noreply.github.com>
    Co-Authored-By: CodeArtist <133830857+beauty-force@users.noreply.github.com>

--- rule-web/src/app/api/events/filter/route.tsx
+++ rule-web/src/app/api/events/filter/route.tsx
@@ -16,17 +16,14 @@ export async function POST(req: NextRequest) {
   try {
     let query = Event.find();
     if (body.upcoming) {
-      console.log('upcoming')
       query.where("eventDate").gte(new Date().getTime());
-      // query.populate({
-      //   path: 'store',
-      //   select: 'storeLat storeLng storeName storeImages address access description status cookingGenre foodGenre storeGenre'
-      // })
-    } else if (body.date) {
-      console.log('date')
-      query.where("eventDate").equals(body.date);
+      query = Event.find({
+        eventDate: { $gte: new Date() }
+      }).populate({
+        path: 'store',
+        select: 'storeLat storeLng storeName storeImages address access description status cookingGenre foodGenre storeGenre'
+      })
     } else if (body.past) {
-      console.log('past')
       // find past events user participated
       if (body.user) {
         // Find all event participations by the user
@@ -52,53 +49,11 @@ export async function POST(req: NextRequest) {
       }
     }
 
-    if (body.category) {
-      console.log('category')
-      query.where("category").equals(body.category);
-    }
-
-    if (body.location) {
-      console.log('location')
-      query.populate({
-        path: 'store',
-        options: { address: body.location },
-        select: 'storeName'
-      });
-    }
-
-    if (body.storeGenre) {
-      console.log('storeGenre')
-      query.populate({
-        path: 'store',
-        match: { storeGenre: body.storeGenre },
-        select: 'storeName'
-      });
-    }
-
-    if (body.foodGenre) {
-      console.log('foodGenre')
-      query.populate({
-        path: 'store',
-        match: { foodGenre: body.foodGenre },
-        select: 'storeName'
-      });
-    }
-
-    if (body.cookingGenre) {
-      console.log('cookingGenre')
-      query.populate({
-        path: 'store',
-        match: { cookingGenre: body.cookingGenre },
-        select: 'storeName'
-      });
-    }
-
     if (body.limit) {
       query.limit(body.limit);
     }
 
     const events = await query.exec();
-    console.log("events: " + JSON.stringify(events))
     return NextResponse.json({ success: true, data: events }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ success: false, error }, { status: 500 });
