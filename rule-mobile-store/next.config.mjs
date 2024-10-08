2b6c26218ef4fa80e919cfc0b6a269957c368867
Genshin <genshindev325@gmail.com>
2024-10-08 17:13:09 -0400

    test
    Co-Authored-By: GENTLE <3442766+gentleman9999@users.noreply.github.com>
    Co-Authored-By: CodeArtist <133830857+beauty-force@users.noreply.github.com>

--- rule-mobile-store/next.config.mjs
+++ rule-mobile-store/next.config.mjs
@@ -4,18 +4,6 @@ const nextConfig = {
   transpilePackages: ["@ionic/react", "@ionic/core", "ionicons"],
   output: "export",
   images: { unoptimized: true },
-  rewrites: () => {
-    return [
-      {
-        source: "/tabs/:path*",
-        destination: "/",
-      },
-      {
-        source: "/event",
-        destination: "/",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
