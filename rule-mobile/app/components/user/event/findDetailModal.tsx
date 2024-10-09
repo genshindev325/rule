88366ed57779fb125978ce554a2828403b0aaae4
Genshin <genshindev325@gmail.com>
2024-10-09 16:02:16 -0400

    test
    Co-Authored-By: GENTLE <3442766+gentleman9999@users.noreply.github.com>
    Co-Authored-By: CodeArtist <133830857+beauty-force@users.noreply.github.com>

--- rule-mobile/app/components/user/event/findDetailModal.tsx
+++ rule-mobile/app/components/user/event/findDetailModal.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useRef, useState } from 'react';
-import { useIonRouter } from '@ionic/react';
+import locations from '@/app/constants/location.json' 
 
 interface EventProps {
   _id: string;
@@ -105,8 +105,11 @@ const FindDetailModal: React.FC<ReviewModalProps> = ({ isOpen, onClose, onSearch
             className={`block flex-1 p-2 bg-transparent border-solid border border-gray-500 rounded-md focus:outline-none ${textXs}`}
           >
             <option value="">場所を選択</option>
-            <option value="Tokyo, Japan">東京、日本</option>
-            <option value="Osaka, Japan">大阪、日本</option>
+            {
+              locations && locations.map((location, index) => (                
+              <option key={index} value={location}>{location}</option>
+              ))
+            }
           </select>
           <input
             type="date"
