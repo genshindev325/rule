03e075354aa6d574738f8e6466bbc42e10d60d65
Genshin <genshindev325@gmail.com>
2024-10-11 12:31:39 -0400

    Update eventList.tsx

--- rule-mobile-store/app/pages/eventList.tsx
+++ rule-mobile-store/app/pages/eventList.tsx
@@ -127,43 +127,45 @@ const EventList: React.FC = () => {
             </IonToolbar>
           </IonHeader>
         <IonContent fullscreen>
-        {/* tab */}
-          <div className='fixed top-14 flex flex-row w-full ion-padding bg-gray-100 pt-6 z-50'>
-            <div className='flex flex-1 justify-center items-center'>
-              <button
-                type='button'
-                className={`${tab === 'upcoming' ? 'bg-blue-400 text-white rounded-lg' : 'bg-transparent text-gray-700'} ${textMd} duration-500 py-1 px-2`}
-                onClick={showUpcomingEvents}
-              >
-                今後のイベント
-              </button>
+          <div className='w-full h-full bg-gray-100'>
+            {/* tab */}
+            <div className='fixed top-11 flex flex-row w-full ion-padding bg-gray-100 pt-8 z-50'>
+              <div className='flex flex-1 justify-center items-center'>
+                <button
+                  type='button'
+                  className={`${tab === 'upcoming' ? 'bg-blue-400 text-white rounded-lg' : 'bg-transparent text-gray-700'} ${textMd} duration-500 py-1 px-2`}
+                  onClick={showUpcomingEvents}
+                >
+                  今後のイベント
+                </button>
+              </div>
+              <div className='flex flex-1 justify-center items-center'>
+                <button
+                  type='button'
+                  className={`${tab === 'past' ? 'bg-blue-400 text-white rounded-lg' : 'bg-transparent text-gray-700'} ${textMd} duration-500 py-1 px-2`}
+                  onClick={showPastEvents}
+                >
+                  過去のイベント
+                </button>
+              </div>
             </div>
-            <div className='flex flex-1 justify-center items-center'>
-              <button
-                type='button'
-                className={`${tab === 'past' ? 'bg-blue-400 text-white rounded-lg' : 'bg-transparent text-gray-700'} ${textMd} duration-500 py-1 px-2`}
-                onClick={showPastEvents}
-              >
-                過去のイベント
-              </button>
-            </div>
-          </div>
-          <div className='min-w-full min-h-[82vh] flex flex-col bg-gray-100 ion-padding pt-20 text-gray-800'>
-            {/* upcoming events */}
-            <div className={`${tab === 'upcoming' ? '' : 'hidden'} space-y-2`}>
-              {upcomingEvents.map((event, index) => (          
-                <div key={index}>
-                  <EventCard { ...event } />
-                </div>
-              ))}
-            </div>
-            {/* past events */}
-            <div className={`${tab === 'past' ? '' : 'hidden'} space-y-2`}>
-              {pastEvents.map((event, index) => (
-                <div key={index}>
-                  <EventReviewCard { ...event } />
-                </div>
-              ))}
+            <div className='w-full flex flex-col bg-gray-100 ion-padding pt-20 pb-4 text-gray-800'>
+              {/* upcoming events */}
+              <div className={`${tab === 'upcoming' ? '' : 'hidden'} space-y-2`}>
+                {upcomingEvents.map((event, index) => (          
+                  <div key={index}>
+                    <EventCard { ...event } />
+                  </div>
+                ))}
+              </div>
+              {/* past events */}
+              <div className={`${tab === 'past' ? '' : 'hidden'} space-y-2`}>
+                {pastEvents.map((event, index) => (
+                  <div key={index}>
+                    <EventReviewCard { ...event } />
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </IonContent>
