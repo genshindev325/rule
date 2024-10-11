2b65831fcfa83aa46e877039a1c5408e70d0ed83
Genshin <genshindev325@gmail.com>
2024-10-11 08:20:14 -0400

    Update unauthorized.tsx
    Co-Authored-By: CodeArtist <133830857+beauty-force@users.noreply.github.com>
    Co-Authored-By: GENTLE <3442766+gentleman9999@users.noreply.github.com>

--- rule-mobile-store/app/pages/auth/unauthorized.tsx
+++ rule-mobile-store/app/pages/auth/unauthorized.tsx
@@ -10,7 +10,7 @@ const Unauthorized = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <div className={`w-screen h-screen flex flex-col items-center justify-center ${maleGradient} text-white text-gray-800`}>
+        <div className={`w-screen h-screen flex flex-col items-center justify-center ${maleGradient} text-white`}>
           <h1 className="text-xl font-bold pb-10">不正アクセス</h1>
           <p className="text-xl font-semibold pb-10">このページを表示する権限がありません。</p>
         </div>
