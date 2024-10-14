18b396807fbb60e58883fafbda9f4e98f307e161
Genshin <genshindev325@gmail.com>
2024-10-14 13:01:40 -0400

    Update reviewModal.tsx

--- rule-mobile-store/app/components/store/dashboard/reviewModal.tsx
+++ rule-mobile-store/app/components/store/dashboard/reviewModal.tsx
@@ -50,7 +50,7 @@ const ReviewModal: React.FC<ReviewModalProps> = ({ isOpen, reviews, onClose, onS
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div ref={modalRef} className="bg-white text-gray-800 p-4 m-4 rounded shadow-md w-full max-w-lg">
-      <h3 className="text-lg font-semibold mb-4">レビュー</h3>
+        <h3 className="text-lg font-semibold mb-4">レビュー</h3>
         <ul>
           {reviews.map((review, index) => (
             <li key={index} className="mb-4">
