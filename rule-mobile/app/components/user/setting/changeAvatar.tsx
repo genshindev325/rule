04bb7702ff147eebab9810ccddadd44af590f376
Genshin <genshindev325@gmail.com>
2024-10-14 12:49:18 -0400

    Update changeAvatar.tsx

--- rule-mobile/app/components/user/setting/changeAvatar.tsx
+++ rule-mobile/app/components/user/setting/changeAvatar.tsx
@@ -14,7 +14,7 @@ const ChangeAvatar: React.FC = () => {
   const maleGradient = 'bg-gradient-to-r from-[#7c5ded] to-[#83d5f7]';
   const textXs = 'text-xs sm:text-sm md:text-md';
   const textSm = 'text-sm md:text-md font-semibold';
-  const input = 'text-xs sm:text-sm md:text-md w-full px-3 sm:px-4 md:px-6 py-2 sm:py-4 border border-gray-700 rounded-md focus:outline-none';
+  const input = 'text-xs sm:text-sm w-full px-3 sm:px-4 py-2 sm:py-3 border border-gray-700 text-gray-800 rounded-md focus:outline-none';
   const [localAvatar, setAvatar] = useState('');
   const router = useIonRouter();
   const token = useSelector((state: RootState) => state.auth.token);
