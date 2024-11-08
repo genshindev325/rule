6412fdcb073720a9d0bd7ee09747004aaf0004ad
Genshin <genshindev325@gmail.com>
2024-11-09 01:04:12 +0900

    Update config.tsx
    Co-Authored-By: GENTLE <3442766+gentleman9999@users.noreply.github.com>
    Co-Authored-By: CodeArtist <133830857+beauty-force@users.noreply.github.com>

--- rule-mobile/app/config.tsx
+++ rule-mobile/app/config.tsx
@@ -1,4 +1,4 @@
 // export const SERVER_URL = 'https://rule-izakaya.vercel.app';
 export const STRIPE_PUBLISHABLE_KEY="pk_test_51PviiUP6SPEuCdw2XGqcDV1e4Xq5jLvyO4sBwiGpxYSHJN2EvnGYVZjvi7Q96ZJA8sxbwQO0ZdbVaaHHgxrk4GSi00DWEXa3Nr";
 export const STRIPE_SECRET_KEY="sk_test_51PviiUP6SPEuCdw2FbMoG4pBsZIIOpWzViNuZtyJajHHMPlUuoR3SGWrjxEB6WASKCnE2UI6RdMk7HJgrxMfz7rw00LLVTpLei";
-export const SERVER_URL = 'http://localhost:3001';
+export const SERVER_URL = 'http://localhost:3000';
