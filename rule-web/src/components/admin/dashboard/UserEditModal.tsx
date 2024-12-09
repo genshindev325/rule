773228b45d89c43f79e97951822b3a91207e6fc3
genshindev325 <genshindev325@gmail.com>
2024-12-09 13:25:56 -0500

    fix(rule-web): fix user edit modal in rule web dashboard
    Co-Authored-By: CodeArtist <133830857+beauty-force@users.noreply.github.com>

--- rule-web/src/components/admin/dashboard/UserEditModal.tsx
+++ rule-web/src/components/admin/dashboard/UserEditModal.tsx
@@ -80,71 +80,61 @@ const UserEditModal: React.FC<IUserEdit> = ({ user, isOpen, onClose, onUserStatu
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 text-gray-800">
       <div ref={modalRef} className="bg-white p-6 rounded shadow-md w-full max-w-xl">
-        <h3 className="text-2xl font-bold mb-4">ユーザー認証</h3>
+        <h3 className="text-2xl font-bold text-center mb-4">ユーザー認証</h3>
+        <h2 className='text-xs text-center mb-10'>登録ユーザーの情報とユーザーIDカードの結果は次のとおりです。</h2>
         <div className='flex flex-row'>
-          <div className='w-1/4 text-md font-semibold'>ユーザーID</div>
-          <div className='w-3/4 text-md'>{user?.userID}</div>
-        </div>
-        <div className='flex flex-row'>
-          <div className='w-1/4 text-md font-semibold'>メール</div>
-          <div className='w-3/4 text-md'>{user?.email}</div>
-        </div>
-        <div className='flex flex-row'>
-          <div className='w-1/4 text-md font-semibold'>ニックネーム</div>
-          <div className='w-3/4 text-md'>{user?.nickname}</div>
-        </div>
-        <div className='flex flex-row'>
-          <div className='w-1/4 text-md font-semibold'>性別</div>
-          <div className='w-3/4 text-md'>{user?.gender === 'male' ? '男' : '女'}</div>
-        </div>
-        <div className='flex flex-row'>
-          <div className='w-1/4 text-md font-semibold'>年齢</div>
-          <div className='w-3/4 text-md'>{age}</div>
-        </div>
-        <div className='flex flex-row'>
-          <div className='w-1/4 text-md font-semibold'>作成日</div>
-          <div className='w-3/4 text-md'>{user?.createdAt && formatDate(user.createdAt.toString())}</div>
-        </div>
-        <div className='flex flex-row'>
-          <div className='w-1/4 text-md font-semibold'>アバター</div>
-          <div className='w-3/4 text-md'>
-            <img src={`${user?.avatar}`} alt='アバター' width={60} height={60} />
+          <div className='w-1/4 text-xs'>
+            <img src={`${user?.verification}`} alt='アバターが選択されていません' />
+          </div>
+          <div className='flex flex-row w-3/4 pl-10'>
+            <div className='flex flex-col items-start w-1/2'>
+              <div className='text-sm'>ユーザーID</div>
+              <div className='text-md font-semibold'>{user?.userID}</div>
+              <div className='text-sm mt-4'>メール</div>
+              <div className='text-md font-semibold'>{user?.email}</div>
+              <div className='text-sm mt-4'>ニックネーム</div>
+              <div className='text-md font-semibold'>{user?.nickname}</div>
+            </div>
+            <div className='flex flex-col items-start w-1/2'>
+              <div className='text-sm'>誕生日</div>
+              <div className='text-md font-semibold'>{user?.birthday ? formatDate(user.birthday.toString()) : ' - - - '}</div>
+              <div className='text-sm mt-4'>年齢</div>
+              <div className='text-md font-semibold'>{age}</div>
+              <div className='text-sm mt-4'>性別</div>
+              <div className='text-md font-semibold'>{user?.gender === 'male' ? '男' : '女'}</div>
+            </div>
           </div>
         </div>
-        <div className='flex flex-row'>
-          <div className='w-1/4 text-md font-semibold'>身分証明書</div>
-          <div className='w-3/4 text-md'>
-            <img src={`${user?.verification}`} alt='アバター' width={200} height={200} />
-          </div>
+        <div className='border border-b-2 border-gray-500 border-dashed mt-4' />
+        <h2 className='text-xs text-left my-4'>アップロードされたID画像</h2>
+        <div className='flex flex-row justify-center w-full px-4 h-60'>
+          <img src={`${user?.verification}`} alt='ユーザーID画像がアップロードされていません' />
         </div>
-        <div className='flex flex-row py-8'>
-          <div className='w-1/4 text-md font-semibold'>状態</div>
-          <div className='flex space-x-4'>
-            <button
-              onClick={() => setSelectedStatus('active')}
-              className={`${
-                selectedStatus === 'active'
-                  ? 'bg-[#26B636] opacity-80 text-white'
-                  : 'text-zinc-850'
-              } p-2 font-semibold focus:outline-none duration-300 rounded-lg text-sm`}
-            >
-              アクティブ
-            </button>
-            <button
-              onClick={() => setSelectedStatus('inactive')}
-              className={`${
-                selectedStatus === 'inactive'
-                  ? 'bg-[#825FD2] opacity-80 text-white'
-                  : 'text-zinc-850'
-              } p-2 font-semibold focus:outline-none duration-300 rounded-lg text-sm`}
-            >
-              非アクティブ
-            </button>
-          </div>
+        <div className='flex flex-row py-4 space-x-4'>
+          <button
+            onClick={() => setSelectedStatus('active')}
+            className={`${
+              selectedStatus === 'active'
+                ? 'bg-[#26B636] opacity-80 text-white'
+                : 'text-zinc-850'
+            } p-1 font-semibold focus:outline-none duration-300 rounded-lg text-xs`}
+          >
+            アクティブ
+          </button>
+          <button
+            onClick={() => setSelectedStatus('inactive')}
+            className={`${
+              selectedStatus === 'inactive'
+                ? 'bg-red-500 opacity-80 text-white'
+                : 'text-zinc-850'
+            } p-1 font-semibold focus:outline-none duration-300 rounded-lg text-xs`}
+          >
+            非アクティブ
+          </button>
         </div>
         <div className='flex flex-row-reverse'>
           <button type='button' className='rounded-md px-4 py-1 bg-gray-400 hover:bg-gray-500 text-white font-bold text-md duration-300' onClick={onClose}>キャンセル</button>
-          <button type='button' className='rounded-md px-10 py-1 mx-6 bg-blue-500 hover:bg-blue-600 text-white font-bold text-md duration-300' onClick={() => setIsSaveConfirmModal(true)}>保存</button>
+          <button type='button' className='rounded-md px-10 py-1 mx-4 bg-blue-500 hover:bg-blue-600 text-white font-bold text-md duration-300' onClick={() => setIsSaveConfirmModal(true)}>保存</button>
         </div>
       </div>
       <SaveConfirmModal isVisible={isSaveConfirmModal} onConfirm={handleConfirmSave} onCancel={() => setIsSaveConfirmModal(false)} />
