6e4dab62e06e0212d7afaac1d7893a6d576949d6
genshindev325 <genshindev325@gmail.com>
2024-12-10 09:26:42 -0500

    Update logout.tsx
    Co-Authored-By: CodeArtist <133830857+beauty-force@users.noreply.github.com>

--- rule-mobile/app/user/auth/logout.tsx
+++ rule-mobile/app/user/auth/logout.tsx
@@ -29,13 +29,10 @@ const LogOut: React.FC = () => {
         <div className="flex items-start justify-center min-h-screen w-screen bg-white text-gray-800">
           <div className={`h-40 sm:h-44 w-full ${maleGradient}`}>
             <div className="bg-white rounded-2xl shadow-xl px-4 sm:px-6 md:px-8 mx-5 sm:mx-6 md:mx-8 mt-12 sm:mt-14 md:mt-16">
-              <h2 className="text-lg font-semibold py-8 sm:py-10 md:py-12 text-center">ログイン</h2>
+              <h2 className="text-lg font-semibold py-8 sm:py-10 md:py-12 text-center">ログアウト</h2>
               <h2 className="mb-8 text-center text-sm text-gray-600">
                 本当にログアウトしますか?
               </h2>
-              <div className={`${textXs} text-right text-gray-400`}>
-                {/* <IonRouterLink routerLink='/auth/loginWith'>ソーシャルログイン</IonRouterLink> */}
-              </div>
               <div className='flex flex-row space-x-4 pb-10'>
                 <button
                   type="button"
